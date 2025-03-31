55bbb969bc50ce1dbb2d8eca5acb2942c4be6c84
Robert Sneddon <30039419@my.nclan.ac.uk>
2025-03-31 20:04:21 +0100

    Added more questions and answers, methods on pages 2,3,4,5,8,9 collect them. Downscaled the images on page 4 but their not centered

--- Services/CollectedAnswers.cs
+++ Services/CollectedAnswers.cs
@@ -7,10 +7,16 @@
         public string Question3 { get; set; } = string.Empty;
         public string Question4 { get; set; } = string.Empty;
         public string Question5 { get; set; } = string.Empty;
+        public string Question6 { get; set; } = string.Empty;
+        public string Question7 { get; set; } = string.Empty;
 
         public string Answer1 {  get; set; } = string.Empty;
         public string Answer2 { get; set; } = string.Empty;
         public string Answer3 { get; set; } = string.Empty;
+        public string Answer4 { get; set; } = string.Empty;
+        public string Answer5 { get; set; } = string.Empty;
+        public string Answer6 { get; set; } = string.Empty;
+        public string Answer7 { get; set; } = string.Empty;
 
         //..
     }
